1eccc5f8ed8ce3041aa753a865bdd58931c70cd0
Saberon, John Paul (JeyP) <johnpaulsaberon1428@gmail.com>
2024-11-10 21:37:16 +0800

    Add files via upload

---
+++ CaIculatorApplication/CaIculatorApplication/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace CaIculatorApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBoxInput1 = new System.Windows.Forms.TextBox();
+            this.txtBoxInput2 = new System.Windows.Forms.TextBox();
+            this.cbOperator = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblDisplayTotal = new System.Windows.Forms.Label();
+            this.btnEqual = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(76, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter First Number:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(52, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(171, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter Second Number:";
+            // 
+            // txtBoxInput1
+            // 
+            this.txtBoxInput1.Location = new System.Drawing.Point(245, 56);
+            this.txtBoxInput1.Name = "txtBoxInput1";
+            this.txtBoxInput1.Size = new System.Drawing.Size(131, 20);
+            this.txtBoxInput1.TabIndex = 3;
+            // 
+            // txtBoxInput2
+            // 
+            this.txtBoxInput2.Location = new System.Drawing.Point(245, 109);
+            this.txtBoxInput2.Name = "txtBoxInput2";
+            this.txtBoxInput2.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxInput2.TabIndex = 4;
+            // 
+            // cbOperator
+            // 
+            this.cbOperator.FormattingEnabled = true;
+            this.cbOperator.Location = new System.Drawing.Point(245, 82);
+            this.cbOperator.Name = "cbOperator";
+            this.cbOperator.Size = new System.Drawing.Size(121, 21);
+            this.cbOperator.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(157, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Answer:";
+            // 
+            // lblDisplayTotal
+            // 
+            this.lblDisplayTotal.AutoSize = true;
+            this.lblDisplayTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDisplayTotal.Location = new System.Drawing.Point(241, 161);
+            this.lblDisplayTotal.Name = "lblDisplayTotal";
+            this.lblDisplayTotal.Size = new System.Drawing.Size(110, 20);
+            this.lblDisplayTotal.TabIndex = 7;
+            this.lblDisplayTotal.Text = "lblDisplayTotal";
+            // 
+            // btnEqual
+            // 
+            this.btnEqual.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEqual.Location = new System.Drawing.Point(214, 206);
+            this.btnEqual.Name = "btnEqual";
+            this.btnEqual.Size = new System.Drawing.Size(162, 36);
+            this.btnEqual.TabIndex = 8;
+            this.btnEqual.Text = "Calculate";
+            this.btnEqual.UseVisualStyleBackColor = true;
+            this.btnEqual.Click += new System.EventHandler(this.btnEqual_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(488, 358);
+            this.Controls.Add(this.btnEqual);
+            this.Controls.Add(this.lblDisplayTotal);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbOperator);
+            this.Controls.Add(this.txtBoxInput2);
+            this.Controls.Add(this.txtBoxInput1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBoxInput1;
+        private System.Windows.Forms.TextBox txtBoxInput2;
+        private System.Windows.Forms.ComboBox cbOperator;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblDisplayTotal;
+        private System.Windows.Forms.Button btnEqual;
+    }
+}
+
---
+++ CaIculatorApplication/CaIculatorApplication/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CaIculatorApplication
+{
+    public delegate double Formula(double num1, double num2);
+
+    public class CalculatorClass
+    {
+        public event Formula CalculateEvent;
+
+        // Methods for addition, subtraction, multiplication, and division
+        public double GetSum(double num1, double num2) => num1 + num2;
+        public double GetDifference(double num1, double num2) => num1 - num2;
+        public double GetProduct(double num1, double num2) => num1 * num2;
+        public double GetQuotient(double num1, double num2) => num2 != 0 ? num1 / num2 : double.NaN; // Handle division by zero
+
+        // Add or remove event handlers
+        public void AddEvent(Formula operation)
+        {
+            CalculateEvent += operation;
+            Console.WriteLine("Added the Delegate");
+        }
+
+        public void RemoveEvent(Formula operation)
+        {
+            CalculateEvent -= operation;
+            Console.WriteLine("Removed the Delegate");
+        }
+    }
+        public partial class Form1 : Form
+    {
+        private CalculatorClass cal;
+        private double num1, num2;
+
+        public Form1()
+        {
+            InitializeComponent();
+            cal = new CalculatorClass();
+
+            // Add items to ComboBox
+            cbOperator.Items.AddRange(new object[] { "+", "-", "*", "/" });
+        }
+
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                num1 = Convert.ToDouble(txtBoxInput1.Text);
+                num2 = Convert.ToDouble(txtBoxInput2.Text);
+            }
+            catch (FormatException)
+            {
+                lblDisplayTotal.Text = "Invalid Input";
+                return;
+            }
+
+            // Determine the operation and perform calculation
+            string selectedOperator = cbOperator.SelectedItem?.ToString();
+
+            if (selectedOperator == "+")
+            {
+                cal.AddEvent(cal.GetSum);
+                lblDisplayTotal.Text = cal.GetSum(num1, num2).ToString();
+                cal.RemoveEvent(cal.GetSum);
+            }
+            else if (selectedOperator == "-")
+            {
+                cal.AddEvent(cal.GetDifference);
+                lblDisplayTotal.Text = cal.GetDifference(num1, num2).ToString();
+                cal.RemoveEvent(cal.GetDifference);
+            }
+            else if (selectedOperator == "*")
+            {
+                cal.AddEvent(cal.GetProduct);
+                lblDisplayTotal.Text = cal.GetProduct(num1, num2).ToString();
+                cal.RemoveEvent(cal.GetProduct);
+            }
+            else if (selectedOperator == "/")
+            {
+                cal.AddEvent(cal.GetQuotient);
+                double result = cal.GetQuotient(num1, num2);
+                lblDisplayTotal.Text = double.IsNaN(result) ? "Cannot divide by zero" : result.ToString();
+                cal.RemoveEvent(cal.GetQuotient);
+            }
+            else
+            {
+                lblDisplayTotal.Text = "Select an operator";
+            }
+        }
+    }
+}
